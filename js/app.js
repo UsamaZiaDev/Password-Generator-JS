c30f6ee088d28ca2414e831cc138d94fb5d08470
usamazia <usama.zia1020@gmail.com>
2022-08-31 17:11:16 +0500

    complete UI Design And work all functionality

--- js/app.js
+++ js/app.js
@@ -1,51 +1,57 @@
+const upperSet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+const lowerSet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+const numberSet = [0,1,2,3,4,5,6,7,8,9]
+const specialChrSet = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "-", ".", "`", "~", "|", "<", ">", "=", "-", "_"]
+
+////////////////////// 
 let newGenValCta = document.querySelector(".newGenCta")
+var inputcountRange, upperCaseVal, lowerCaseVal, specialChrVal, numberVal, showRandonTextVal;
 
-newGenValCta.addEventListener("click",function(e){
-    e.preventDefault();
-    ////////////////////// 
-    let inputcountRange = document.querySelector(".countRange").value
-    let upperCaseVal = document.querySelector(".upperCaseVal").checked
-    let lowerCaseVal = document.querySelector(".lowerCaseVal").checked
-    let specialChrVal = document.querySelector(".specialChrVal").checked
-    let numberVal = document.querySelector(".numberVal").checked
-    // let copyTextCta = document.querySelector(".copyTextCta")
+// let resultPassword = "";
+let char = 0;
 
-    var demoary = [];
+newGenValCta.addEventListener("click",function(){
 
-    // Validate Uppercase Text Condition
-    if(upperCaseVal === true){
-        for(i=65; i<=90; i++){
-            demoary.push(String.fromCharCode(i))
-        }
+    inputcountRange = document.querySelector(".countRange").value
+    upperCaseVal = document.querySelector(".upperCaseVal").checked
+    lowerCaseVal = document.querySelector(".lowerCaseVal").checked
+    specialChrVal = document.querySelector(".specialChrVal").checked
+    numberVal = document.querySelector(".numberVal").checked
+    showRandonTextVal = document.querySelector(".randonTextVal")
+
+    if(inputcountRange<4){
+        alert("min val 4")
+    }else if(inputcountRange>15){
+        alert("max val 15")
+    }else{
+        genratePass()
     }
-
-    // Validate lowerCaseVal Text Condition
-     if(lowerCaseVal === true){
-        for(i=97; i<= 122; i++){
-            demoary.push(String.fromCharCode(i))
-        }
-    }
-
-
-    // Validate specialChrVal Condition
-     if(specialChrVal === true){
-        for(i=33; i<=47; i++){
-            demoary.push(String.fromCharCode(i))
-        }
-        for(i=58; i<=63; i++){
-            demoary.push(String.fromCharCode(i))
-        }
-        for(i=123; i<=127; i++){
-            demoary.push(String.fromCharCode(i))
-        }
-    }
-
-
-     if(numberVal === true){
-        for(i=48; i<=57; i++){
-            demoary.push(String.fromCharCode(i))
-        }
-    }
-
-
+    
 })
+
+
+function genratePass(){
+    let resultPassword = "";
+
+    for(i=0; i<inputcountRange; i++){
+
+        if(char !== inputcountRange){
+    
+            if(lowerCaseVal){
+                resultPassword += lowerSet[Math.floor(Math.random()* 25)]
+            }
+            if(upperCaseVal){
+                resultPassword += upperSet[Math.floor(Math.random()* 25)]
+            }
+            if(specialChrVal){
+                resultPassword += specialChrSet[Math.floor(Math.random()* 21)]
+            }
+            if(numberVal){
+                resultPassword += numberSet[Math.floor(Math.random()* 10)]
+            }
+            char = char + 1;
+        }
+        resultPassword = resultPassword.slice(0, inputcountRange)
+        showRandonTextVal.innerHTML = resultPassword
+    }
+}
